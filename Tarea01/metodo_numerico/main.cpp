35a4ac5f1c17e4656b30c2d34450d18acded7072
artavias <andres_artavia01@hotmail.com>
2022-09-16 02:01:13 -0600

    implementacion de metodos y herencia

--- Tarea01/metodo_numerico/main.cpp
+++ Tarea01/metodo_numerico/main.cpp
@@ -1,35 +1,18 @@
 #include <iostream>
-#include <cmath>
+#include "newton_raphson.h"
 
 using namespace std;
 
-//Se define la clase funcion para evaluar la funcion y su derivada
-class funcion
-{
-public:
-
-    //El siguiente metodo de la clase retorna el valor al evaluar la funcion en un valor x
-    float resolverEcuacion (float x)
-    {
-        return pow(x, 3) + 3*pow(x, 2) - 8*x + 12;
-    }
-
-     //El siguiente metodo de la clase retorna el valor al evaluar la derivada en un valor x
-    float resolverDerivada ( float x)
-    {
-        return 3*pow(x, 2) + 6*x - 8;
-    }
-};
-
 int main()
 {
-    funcion f;
-    float a = f.resolverEcuacion(4);
-    float b = f.resolverDerivada(4);
 
-    cout<<a<<'\n'<<b<<endl;
+    newton a;
 
-    return 0;
+
+
+    cout<<a.newton_raphsone(0.75)<<endl;
+
 }
+
 
 
--- Tarea01/metodo_numerico/newton_raphson.h
+++ Tarea01/metodo_numerico/newton_raphson.h
@@ -1,1 +1,66 @@
+#include <cmath>
+#include <stdio.h>
 
+using namespace std;
+
+//Se define la clase funcion para evaluar la funcion y su derivada
+class funcion
+{
+    public:
+
+    //El siguiente metodo de la clase retorna el valor al evaluar la funcion en un valor x
+    float evaluarFuncion (float x)
+    {
+        return pow(x, 3) + 4*pow(x, 2) - 10;
+    }
+
+     //El siguiente metodo de la clase retorna el valor al evaluar la derivada en un valor x
+    float evaluarDerivada (float x)
+    {
+        return pow((3*x), 2) + 8*x;
+    }
+
+    //El siguiente metodo utiliza los metodos de funcion para calcular el siguiente valor de x
+    float calcular_siguiente (float x)
+    {
+        return x - (evaluarFuncion(x)/evaluarDerivada(x));
+    }
+
+
+};
+
+
+class error: public funcion
+{
+    public:
+
+    float calcular_error (float x)
+    {
+        return abs(calcular_siguiente(x) - x);
+    }
+};
+
+class newton: public error
+{
+    public:
+
+    float newton_raphsone(float x)
+    {
+        int iterador = 0;
+        float error = 0;
+        float x1 = x;
+        float x2 = 0;
+        while(true)
+        {
+            x2 = calcular_siguiente(x1);
+            iterador++;
+            error = calcular_error(x1);
+            if (error == 0)
+            {
+                break;
+            }
+            x1=x2;
+        }
+        return x2;
+    }
+};
